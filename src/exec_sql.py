c3eb84477a4af5b5b746adc905dca119563d5147
youkidkk <youkidkk80@gmail.com>
2022-07-03 11:32:19 +0900

    初回コミット

---
+++ src/exec_sql.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+
+DATABASE = "postgresql"
+USER = "pysqlal"
+PASSWORD = "pysqlal"
+HOST = "localhost"
+PORT = "5432"
+DB_NAME = "pysqlal"
+
+CONN_STR = f"{DATABASE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}"
+
+engine = sqlalchemy.create_engine(CONN_STR)
+
+engine.execute("insert into test values (1, 'ttt');")
